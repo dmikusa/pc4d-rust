48b683e20c6aea4eba3bcc91960a504075da895d
Daniel Mikusa <dan@mikusa.com>
2020-01-21 15:06:15 -0500

    Added some comments & code clarity improvements

--- src/bin/encrypt/main.rs
+++ src/bin/encrypt/main.rs
@@ -8,17 +8,29 @@ fn main() {
         std::process::exit(1);
     }
 
+    sodiumoxide::init().expect("Cannot initialize Sodium Oxide/libsodium");
+
+    // generate a key
     let key = secretbox::gen_key();
+
+    // generate a nonce
     let nonce = secretbox::gen_nonce();
+
+    // get message
     let message = env::args().nth(1).unwrap_or_default();
     
+    // encrypt
     let ciphertext = secretbox::seal(message.as_bytes(), &nonce, &key);
-    let secretbox:Vec<u8> = nonce.as_ref().iter().map(|x| *x).chain(ciphertext.into_iter()).collect();
 
-    println!("Nonce: {}", hex::encode(nonce.as_ref()));
-    println!("Key: {}", hex::encode(key.as_ref()));
+    // combine the nonce & cipher text
+    let mut secretbox:Vec<u8> = vec![];
+    secretbox.extend(nonce.as_ref());
+    secretbox.extend(ciphertext);
+
+    println!("Nonce: {}", hex::encode(&nonce));
+    println!("Key: {}", hex::encode(&key));
     println!("Box: {}", base64::encode(&secretbox));
     println!();
-    println!("Decrypt with: debug/target/decrypt {} {}", hex::encode(key), base64::encode(&secretbox));
+    println!("Decrypt with: debug/target/decrypt {} {}", hex::encode(&key), base64::encode(&secretbox));
     println!();
 }
