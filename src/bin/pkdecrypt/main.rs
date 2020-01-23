0024af93954d3c6f42f9af2e604ead84bc9f3195
Daniel Mikusa <dan@mikusa.com>
2020-01-22 23:54:40 -0500

    Added implementations of pkencrypt & pkdecrypt

--- src/bin/pkdecrypt/main.rs
+++ src/bin/pkdecrypt/main.rs
@@ -1,3 +1,47 @@
+use sodiumoxide::crypto::box_;
+use std::env;
+
 fn main() {
-    println!("decrypt");
+    if env::args().count() != 4 {
+        println!(
+            "Usage: {} senderPublicKey recipientPrivateKey box",
+            env::args().nth(0).unwrap()
+        );
+        println!("    senderPublicKey: base-64 encoded public key");
+        println!("    recipientPrivateKey: base64-encoded private key");
+        println!("    box: base64 encoded encrypted data");
+        std::process::exit(1);
+    }
+
+    sodiumoxide::init().expect("Cannot initialize Sodium Oxide/libsodium");
+
+    // get sender's public key
+    let sender_pk = box_::PublicKey::from_slice(
+        &base64::decode(&env::args().nth(1).unwrap_or_default()).expect("invalid base64"),
+    )
+    .expect("invalid public key");
+
+    // get receipient's private key
+    let receiver_sk = box_::SecretKey::from_slice(
+        &base64::decode(&env::args().nth(2).unwrap_or_default()).expect("invalid base64"),
+    )
+    .expect("invalid private key");
+
+    // get the secret box
+    let secret = base64::decode(&env::args().nth(3).unwrap_or_default()).expect("invalid base64");
+
+    // regenerate the nonce
+    let nonce = box_::Nonce::from_slice(&secret[0..box_::NONCEBYTES]).expect("invalid nonce");
+    let ciphertext = &secret[box_::NONCEBYTES..];
+
+    // decrypt
+    println!("Nonce: {}", hex::encode(&nonce));
+    if let Ok(message) = box_::open(ciphertext, &nonce, &sender_pk, &receiver_sk) {
+        println!(
+            "Message: {}",
+            String::from_utf8(message).expect("Invalid utf-8")
+        );
+    } else {
+        println!("Decryption failed :(");
+    }
 }
--- src/bin/pkencrypt/main.rs
+++ src/bin/pkencrypt/main.rs
@@ -1,3 +1,45 @@
+use sodiumoxide::crypto::box_;
+use std::env;
+
 fn main() {
-    println!("decrypt");
+    if env::args().count() != 2 {
+        println!("Usage: {} plaintext", env::args().nth(0).unwrap());
+        println!("    plaintext: unencrypted data");
+        std::process::exit(1);
+    }
+
+    sodiumoxide::init().expect("Cannot initialize Sodium Oxide/libsodium");
+
+    // generate public/private keypairs
+    let (sender_pk, sender_sk) = box_::gen_keypair();
+    let (receiver_pk, receiver_sk) = box_::gen_keypair();
+
+    // generate a nonce
+    let nonce = box_::gen_nonce();
+
+    // get message
+    let message = env::args().nth(1).unwrap_or_default();
+
+    // encrypt
+    let ciphertext = box_::seal(message.as_bytes(), &nonce, &receiver_pk, &sender_sk);
+
+    // combine the nonce & cipher text
+    let mut secret: Vec<u8> = vec![];
+    secret.extend(nonce.as_ref());
+    secret.extend(ciphertext);
+
+    println!("Sender public key: {}", base64::encode(&sender_pk));
+    println!("Sender private key: {}", base64::encode(&sender_sk));
+    println!("Recipient public key: {}", base64::encode(&receiver_pk));
+    println!("Recipient private key: {}", base64::encode(&receiver_sk));
+    println!("Nonce: {}", hex::encode(&nonce));
+    println!("Box: {}", base64::encode(&secret));
+    println!();
+    println!(
+        "Decrypt with: target/debug/pkdecrypt {} {} {}",
+        base64::encode(&sender_pk),
+        base64::encode(&receiver_sk),
+        base64::encode(&secret)
+    );
+    println!();
 }
