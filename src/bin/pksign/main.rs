370ba73098304e1cf08601923a5b556a61d7ba1a
Daniel Mikusa <dan@mikusa.com>
2020-01-23 00:08:53 -0500

    Added implementations of pksign & pkverify.

--- src/bin/pksign/main.rs
+++ src/bin/pksign/main.rs
@@ -1,3 +1,32 @@
+use sodiumoxide::crypto::sign;
+use std::env;
+
 fn main() {
-    println!("decrypt");
+    if env::args().count() != 2 {
+        println!("Usage: {} message", env::args().nth(0).unwrap());
+        println!("    message: message to sign");
+        std::process::exit(1);
+    }
+
+    sodiumoxide::init().expect("Cannot initialize Sodium Oxide/libsodium");
+
+    // generate public/private key
+    let (public_key, private_key) = sign::gen_keypair();
+
+    // get message
+    let message = env::args().nth(1).unwrap_or_default();
+
+    // sign it
+    let signature = sign::sign(message.as_bytes(), &private_key);
+
+    println!("Public Key: {}", hex::encode(&public_key));
+    println!("Private Key: {}", hex::encode(&private_key));
+    println!("Signature: {}", base64::encode(&signature));
+    println!();
+    println!(
+        "Verify with: target/debug/pkverify {} {}",
+        hex::encode(&public_key),
+        base64::encode(&signature)
+    );
+    println!();
 }
--- src/bin/pkverify/main.rs
+++ src/bin/pkverify/main.rs
@@ -1,3 +1,32 @@
+use sodiumoxide::crypto::sign;
+use std::env;
+
 fn main() {
-    println!("decrypt");
+    if env::args().count() != 3 {
+        println!(
+            "Usage: {} publicKey signedMessage",
+            env::args().nth(0).unwrap()
+        );
+        println!("    publicKey: hex-encoded public key");
+        println!("    signedMessage: base64-encoded signed message");
+        std::process::exit(1);
+    }
+
+    sodiumoxide::init().expect("Cannot initialize Sodium Oxide/libsodium");
+
+    // get sender's public key
+    let signer_pk = sign::PublicKey::from_slice(
+        &hex::decode(&env::args().nth(1).unwrap_or_default()).expect("invalid hex"),
+    )
+    .expect("invalid public key");
+
+    // get message
+    let signed_message =
+        base64::decode(&env::args().nth(2).unwrap_or_default()).expect("invalid base64");
+
+    // verify
+    match sign::verify(&signed_message, &signer_pk) {
+        Ok(_) => println!("Verified"),
+        Err(_) => println!("NOT Verified"),
+    }
 }
